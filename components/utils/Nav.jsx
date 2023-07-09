aac34056212791f28fa49c4e47cfe70e95f6b556
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2023-07-09 04:51:47 +0200

    Update Nav.jsx

--- components/utils/Nav.jsx
+++ components/utils/Nav.jsx
@@ -1,17 +1,20 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { useContext } from "react";
 
 import Settings from "../settings/Settings";
 import Search from "../search/Search";
+import { ColorContext } from "./ColorContext";
 import styles from '../../styles';
 
 const Tab = createBottomTabNavigator();
 
 export default function Nav() {
+    const { theme } = useContext(ColorContext);
     return (
         <Tab.Navigator 
             screenOptions={{
-            tabBarStyle: { backgroundColor: styles.color, borderColor: "#3f101c", padding: 10 },
+            tabBarStyle: { backgroundColor: theme, borderColor: "#3f101c", padding: 10 },
             tabBarActiveTintColor: 'white',
             }}
             >
@@ -22,7 +25,7 @@ export default function Nav() {
                     tabBarIcon: ({ size }) => (
                         <Icon name="search" color="white" size={size} />
                         ),
-                        headerStyle: { backgroundColor: styles.color }, tabBarLabel: '', headerTintColor: 'white'
+                        headerStyle: { backgroundColor: theme}, tabBarLabel: '', headerTintColor: 'white'
                     }}
             />
             <Tab.Screen
@@ -31,7 +34,7 @@ export default function Nav() {
                 options={{
                 tabBarIcon: ({ size }) => (
                     <Icon name="cog" color="white" size={size} />
-                    ), headerStyle: { backgroundColor: styles.color }, tabBarLabel: '', headerTintColor: 'white'
+                    ), headerStyle: { backgroundColor: theme }, tabBarLabel: '', headerTintColor: 'white'
                 }}
             />
         </Tab.Navigator>
