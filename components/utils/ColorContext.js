f91ce5101da478c3a9929b81b03adf1962f5e37b
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2023-07-11 02:08:26 +0200

    Update ColorContext.js

--- components/utils/ColorContext.js
+++ components/utils/ColorContext.js
@@ -1,13 +1,1 @@
-import { createContext, useState } from "react";
 
-import styles from "../../styles";
-
-export const ColorContext = createContext()
-export function ColorProvider({ children }) {
-    const [theme, setTheme] = useState(styles.color);
-    return (
-        <ColorContext.Provider value={{ theme, setTheme }}>
-          {children}
-        </ColorContext.Provider>
-    );
-}
